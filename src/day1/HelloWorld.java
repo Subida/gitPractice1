232f70293e69407259cdd73ef0801ef787ed5f35
Reyimujiang Subidan <subidareyimujiang@gmail.com>
2021-04-24 16:56:07 -0400

    initial commit added more

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("yay stuff are on github");
+        System.out.println("adding final wrap up code");
     }
 
 
